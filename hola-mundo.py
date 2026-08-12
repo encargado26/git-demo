075f785f8edfbecc8c482b2162375d3cf77a2ed0
encargado26 <caceresparedmarceloagustin@gmail.com>
2026-08-12 17:43:34 -0300

    .

--- hola-mundo.py
+++ hola-mundo.py
@@ -1,4 +1,4 @@
 print("Hola Mundo Buenas Tardes")
 print("Como estan Señores y Señoras?")
 print("Que Cuentas Tienen?")
-print("Gracias a Todos! , Hasta la Proxima!!")
+print("Gracias a Todos! , Hasta la Proxima!")
